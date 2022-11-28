183ccf10349ba29c939e673e8006d6a40d1925c1
joker876 <joker876x@gmail.com>
2022-11-28 21:08:47 +0100

    add JSX comments and README

---
+++ README.md
@@ -0,0 +1,93 @@
+# decimal-separator
+
+Find the decimal and group separators used in any given locale.
+
+## Highlights
+* Supports TypeScript!
+* Supports Node and browser
+* Includes full JSX documentation
+* Very lightweight!
+
+
+## Installation
+### NodeJS
+```
+npm install decimal-separator --save
+```
+
+### Browser
+Import the script:
+```html
+<script src="https://joker876.github.io/decimal-separator/decimal-separator.min.js">
+```
+
+## Usage
+### NodeJS
+```typescript
+import * from 'decimal-separator';
+// or
+import { /* function names here */ } from 'decimal-separator';
+```
+
+### Browser
+```js
+DecimalSep.decimalSep();
+// or
+const { decimalSep, groupSep, ... } = DecimalSep;
+```
+
+## Exported functions
+### decimalSep
+```typescript
+function decimalSep(): string;
+function decimalSep(locale: string): string;
+```
+Get the decimal separator for the given locale.
+
+* `locale` - The locale to get the decimal separator from. Optional. If not given, uses the current user's locale.
+
+Returns a string containing the decimal separator.
+
+### groupSep
+```typescript
+function groupSep(): string;
+function groupSep(locale: string): string;
+```
+Get the group separator for the given locale.
+
+* `locale` - The locale to get the group separator from. Optional. If not given, uses the current user's locale.
+
+Returns a string containing the group separator.
+
+### formatUsing
+```typescript
+function formatUsing(num: number, decimal: string): string;
+function formatUsing(num: number, decimal: string, group: string): string;
+```
+Formats a number using the given decimal and group separators.
+
+* `num` - The number to format.
+* `decimal` - The decimal separator to use.
+* `group` - The group separator to use. Optional, defaults to "`.`".
+
+Returns a string containing the number, formatted using the given separators.
+
+### formatWithLocale
+```typescript
+function formatWithLocale(num: number): string;
+function formatWithLocale(num: number, locale: string): string;
+```
+Formats a number using the given locale.
+
+* `num` - The number to format.
+* `locale` - The locale to use to format the number. Optional. If not given, uses the current user's locale.
+
+Returns the number formatted using the given locale.
+
+## Changelog
+### 1.1.0
+* Added JSX documentation.
+* Added this README.
+
+### 1.0.0
+Initial release.
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,29 @@
 
+/**
+ * Get the decimal separator for the current user's locale.
+ * @returns A string containing the decimal separator.
+ */
 export function decimalSep(): string;
+/**
+ * Get the decimal separator for the given locale.
+ * @param locale The locale to get the decimal separator from.
+ * @returns A string containing the decimal separator.
+ */
 export function decimalSep(locale: string): string;
 export function decimalSep(locale?: string): string {
     return (1.1).toLocaleString(locale).charAt(1);
 }
 
+/**
+ * Get the group separator for current user's locale.
+ * @returns A string containing the group separator.
+ */
 export function groupSep(): string;
+/**
+ * Get the group separator for the given locale.
+ * @param locale The locale to get the group separator from.
+ * @returns A string containing the group separator.
+ */
 export function groupSep(locale: string): string;
 export function groupSep(locale?: string): string {
     let sep = (1000).toLocaleString(locale).charAt(1);
@@ -13,7 +31,20 @@ export function groupSep(locale?: string): string {
     return sep;
 }
 
+/**
+ * Formats a number using the given decimal separator.
+ * @param num The number to format.
+ * @param decimal The decimal separator to use.
+ * @returns A string containing the number, formatted using the given separator.
+ */
 export function formatUsing(num: number, decimal: string): string;
+/**
+ * Formats a number using the given decimal and group separators.
+ * @param num The number to format.
+ * @param decimal The decimal separator to use.
+ * @param group The group separator to use.
+ * @returns A string containing the number, formatted using the given separator.
+ */
 export function formatUsing(num: number, decimal: string, group: string): string;
 export function formatUsing(num: number, decimal: string, group?: string): string {
     if (typeof num != 'number') throw new TypeError(`Expected argument 1 of formatUsing to be number, got ${typeof num}`);
@@ -26,8 +57,18 @@ export function formatUsing(num: number, decimal: string, group?: string): strin
     //join them back and return
     return split.join(decimal ?? '.');
 }
-
+/**
+ * Formats a number using the current user's locale.
+ * @param num The number to format using the given locale.
+ * @returns The number formatted using the locale.
+ */
 export function formatWithLocale(num: number): string;
+/**
+ * Formats a number using the given locale.
+ * @param num The number to format using the given locale.
+ * @param locale The locale to use to format the number.
+ * @returns The number formatted using the locale.
+ */
 export function formatWithLocale(num: number, locale: string): string;
 export function formatWithLocale(num: number, locale?: string): string {
     if (typeof num != 'number') throw new TypeError(`Expected argument 1 of formatWithLocale to be number, got ${typeof num}`);
